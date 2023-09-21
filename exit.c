3660020ce677a30667f3fb244e29d73d1561dca4
EnwonoEdo <promiseedoinyang@gmail.com>
2023-09-21 03:39:03 -0700

    Simple Shell Tasks

---
+++ exit.c
@@ -0,0 +1,73 @@
+#include "shell.h"
+
+/**
+ **_strncpy - copies a string up to 'n' characters
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the maximum number of characters to be copied
+ *Return: A pointer to the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - concatenates two strings up to 'n' characters
+ *@dest: the first string
+ *@src: the second string
+ *@n: the maximum number of bytes to use
+ *Return: A pointer to the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: A pointer to the memory area with 'c' in 's' or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+	return (NULL);
+}
